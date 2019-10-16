3be39585c7f143651ae1bab9fe3a471fd73052eb
CRahne <coleprahne@gmail.com>
2019-10-15 22:49:06 -0500

    Fixed Bad Code from before

--- GUI_For_PI.py
+++ GUI_For_PI.py
@@ -1,6 +1,10 @@
 from tkinter import *
 import time
-import keyboard
+import keyboard # Tools -> Manage Packages -> keyboard -> Install
+
+# Also I found a good video of how to do something
+# similar to what we are doing:
+# https://www.youtube.com/watch?v=dQw4w9WgXcQ
 
  
 root = Tk()
@@ -8,41 +12,84 @@ root.geometry('1000x500+20+20')
  
 mainframe = Frame(root)
 mainframe.grid(column=1000, row=1000, sticky=(N, W, E, S))
-mainframe.columnconfigure(0, weight=1)
-mainframe.rowconfigure(0, weight=1)
+
+mainframe.columnconfigure(3, weight=100)
+mainframe.rowconfigure(3, weight=100)
+
+Font_Size = 12
+
+# Row 0
+
+Title_String = StringVar()
+Title_String.set("This is my great title")
+
+Title_Label = Label(mainframe,textvariable=Title_String,bg='#321000',fg='#000fff000',font=("Helvetica",Font_Size))
+Title_Label.grid(row=0, column=1, padx=50)
  
-best = StringVar()
-best.set('start')
+Team_Number = StringVar()
+Team_Number.set("2509")
 
-best2 = StringVar()
-best2.set('start2')
+Team_Number_L = Label(mainframe,textvariable=Team_Number,bg='#321000',fg='#000fff000',font=("Helvetica",Font_Size))
+Team_Number_L.grid(row=0, column=0, padx=50)
+ 
+Team_Number_R = Label(mainframe,textvariable=Team_Number,bg='#321000',fg='#000fff000',font=("Helvetica",Font_Size))
+Team_Number_R.grid(row=0, column=2, padx=50)
 
-best3 = StringVar()
-best3.set("image")
+# Row 1
 
-x1 = 12
+Answer1 = StringVar()
+Answer1.set('Woofer')
 
-label1 = Label(mainframe,textvariable=best,bg='#321000',fg='#000fff000',font=("Helvetica",x1))
-label1.grid(row=1, column=0, padx=50)
+Answer1_Label = Label(mainframe,textvariable=Answer1,bg='#321000',fg='#000fff000',font=("Helvetica",Font_Size))
+Answer1_Label.grid(row=1, column=0, padx=50)
 
-label3 = Label(mainframe,textvariable=best3,bg='#321000',fg='#000fff000',font=("Helvetica",x1))
-label3.grid(row=1, column=1,padx=50)
+Question = StringVar()
+Question.set('Which animal')
 
-label2 = Label(mainframe,textvariable=best2,bg='#321000',fg='#000fff000',font=("Helvetica",x1))
-label2.grid(row=1,column=2,padx=50)
+Question_Label = Label(mainframe,textvariable=Question,bg='#321000',fg='#000fff000',font=("Helvetica",Font_Size))
+Question_Label.grid(row=1, column=1,padx=50)
+
+Answer2 = StringVar()
+Answer2.set("Meower")
+
+Answer2_Label = Label(mainframe,textvariable=Answer2,bg='#321000',fg='#000fff000',font=("Helvetica",Font_Size))
+Answer2_Label.grid(row=1,column=2,padx=50)
+
+ 
+Count1 = StringVar()
+Count1.set('start')
+
+Count1_Label = Label(mainframe,textvariable=Count1,bg='#321000',fg='#000fff000',font=("Helvetica",Font_Size))
+Count1_Label.grid(row=2, column=0, padx=50)
+
+Middle = StringVar()
+Middle.set('start2')
+
+Middle_Label = Label(mainframe,textvariable=Middle,bg='#321000',fg='#000fff000',font=("Helvetica",Font_Size))
+Middle_Label.grid(row=2, column=1,padx=50)
+
+Count2 = StringVar()
+Count2.set("image")
+
+Count2_Label = Label(mainframe,textvariable=Count2,bg='#321000',fg='#000fff000',font=("Helvetica",Font_Size))
+Count2_Label.grid(row=2,column=2,padx=50)
  
 def run(n, i, master):
-    best.set('test # %d' % (n))
-    best2.set(f'test2 # {i}')
+    Count1.set('%d' % (n))
+    Count2.set('%d' % (i))
     if keyboard.is_pressed('q'):
         n += 1
+        time.sleep(0.1)
     if keyboard.is_pressed('w'):
         i += 1
+        time.sleep(0.1)
     if n <= 10 or i <= 10:
-        mainframe.after(100, run, n, i, master)
+        mainframe.after(1, run, n, i, master)
+    else:
+        i = 0
+        n = 0
+        Title_String.set("Wow U R Cool")
+        mainframe.after(1, run, n, i, master)
 
 run(0, 0, mainframe)
 root.mainloop()
-while True:
-    best.set('testestes')
-    root.update()
